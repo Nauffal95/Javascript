8cb7fcc634a8347fbe4b29d1503a69837a1f06d8
Nauffal95 <cnauffal@outlook.com>
2022-03-18 11:45:00 +0100

    nouveau chang

--- sololearn.js
+++ sololearn.js
@@ -3,5 +3,5 @@ const img = document.getElementById('img');
 
 btn.addEventListener ( "click" , () => {img.classList.toggle("show")})
 console.log('click');
-
+// nouveau changement
 // ..btn.addEventListener("click", () => { img.classList.add ("show")})
