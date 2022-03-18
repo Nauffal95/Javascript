a928d3664281a7c958a3b1c3febb5e0a97067004
Nauffal95 <cnauffal@outlook.com>
2022-03-18 11:42:11 +0100

    finale

--- sololearn.js
+++ sololearn.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector("#btn");
 const img = document.getElementById('img');
 
-btn.addEventListener ("clic" , () => {img.classList.add("show")})
+btn.addEventListener ( "click" , () => {img.classList.toggle("show")})
 console.log('click');
 
-// btn.addEventListener("click", () => { img.classList.add ("show")})
+// ..btn.addEventListener("click", () => { img.classList.add ("show")})
